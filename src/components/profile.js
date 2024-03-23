a6351848c2f5f69527e66db3f9a730e8606a949d
Tomoya HAYASHI <114045084+k20rs097@users.noreply.github.com>
2024-03-23 12:19:34 +0900

    Update
    プロフィールをgrid-layoutをしようして記述。

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,8 +1,10 @@
 const Profile = () => {
     return (
-        <section id="profile" className="profile">
+        <section id="profile" className="grid-wrapper">
             <h1>ABOUT ME</h1>
-            
+            <div>aaaaaa</div>
+            <div>bbbbbb</div>
+            <div>cccccc</div>
         </section>
     );
 }
