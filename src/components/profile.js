6636cfec911b25b75aa9e07e7729011d252a5c72
Tomoya HAYASHI <114045084+k20rs097@users.noreply.github.com>
2024-03-23 13:26:05 +0900

    Update

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,10 +1,23 @@
 const Profile = () => {
     return (
-        <section id="profile" className="grid-wrapper">
-            <h1>ABOUT ME</h1>
-            <div>aaaaaa</div>
-            <div>bbbbbb</div>
-            <div>cccccc</div>
+        <section id="profile" className="profile">
+            <h1 className="content-titles">ABOUT ME</h1>
+            <div className="grid-wrapper">
+                <div className="grid-content--image">
+                    <img
+                        className="fit-picture"
+                        src="./media/img/topIcon.png"
+                        alt="An Otter Icon" />
+                </div>
+                <div className="grid-content--title">Hayashi Tomoya</div>
+                <div className="grid-content--title">
+                    <p>
+                        2001年12月21日生まれ。<br />
+                        エンジニアを目指して勉強・就活中。<br />
+                        好きな食べ物はカレーです。
+                    </p>
+                </div>
+            </div>
         </section>
     );
 }
